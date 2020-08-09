1c27dc40d5b453efc0d8f42626fabc01f759dcff
BlaiseKufo <nassim.hafici@gmail.com>
2020-08-09 19:39:02 -0400

    Module 3 test

--- jobs/app.py
+++ jobs/app.py
@@ -1,13 +1,15 @@
 import flask
-from flask import Flask, render_template
-
-
-
+from flask import Flask, render_template,g
+import sqlite3
 app = Flask(__name__)
+
+def open_connection:
+    connection = getattr(g,'_database',None)
+    return connection 
 @app.route('/')
 @app.route('/jobs')
 
-
 def jobs():
-
     return render_template('index.html')
+
+PATH = "db/jobs.sqlite"
